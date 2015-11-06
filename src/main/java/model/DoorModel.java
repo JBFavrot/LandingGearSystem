16ac2a1b9601e823415e4071e28fd3f24694661d
JBFavrot <jeanbaptiste.favrot@outlook.fr>
2015-11-06 14:20:05 +0100

    DoorModel implementation

--- src/main/java/model/DoorModel.java
+++ src/main/java/model/DoorModel.java
@@ -1,5 +1,29 @@
 package model;
 
 public class DoorModel {
+    private static final int OPEN = 1;
+    private static final int CLOSED = 0;
 
+    int doorState = CLOSED;
+
+    /**
+     * Changes the door state
+     * @return new door state
+     */
+    public int changeDoorState()
+    {
+        if (doorState==CLOSED) doorState=OPEN;
+        else doorState=CLOSED;
+        return doorState;
+
+    }
+
+    /**
+     *
+     * @return Door state
+     */
+    public int getDoorState()
+    {
+        return doorState;
+    }
 }
